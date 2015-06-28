787ec8afa01a0b7cf0d4345e83a15df5f4456e2b
NewWorldOrder <nwokedi@gmail.com>
2015-06-27 23:55:29 -0700

    cleaned up directory structure and made mapped changes too

--- sci_eng_data.R
+++ sci_eng_data.R
@@ -1,9 +1,13 @@
 # read in the headings for this dataset 
-headings <- read.xlsx("tab5-6.xlsx", 1, startRow = 3, endRow = 3, header = F)
+headings <- read.xlsx("data/tab5-6.xlsx", 1, startRow = 3, endRow = 3, header = F)
 headings[1] <- "field"
 # read in the data for whites 
-x <- read.xlsx("tab5-6.xlsx", 1, startRow = 6, endRow = 50, header = F)
+x <- read.xlsx("data/tab5-6.xlsx", 1, startRow = 6, endRow = 50, header = F)
 colnames(x) <- headings
 whites <- x[!is.na(x$field),]
 # plot trend over years 
 plot(colnames(whites[,-1]), whites[6, -1], type = "b", col = "red", main = "% of CS Bachelor Degrees Awarded \nto Whites")
+
+# Read in data for each other race
+
+# Plot these separately and together 
